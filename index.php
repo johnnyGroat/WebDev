06e6c5b046211368436d2e6b4e778c80ca9bb1c1
Johnny Groat <johnathan.groat@gmail.com>
2015-07-18 16:31:51 -0400

    link bootstrap js and css files to the project

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <html>
 	<head>
-		<?php include 'header.html';?>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js" ></script>
+		<link  rel="stylesheet" type="text/css" href="content/bootstrap/css/bootstrap.min.css" />
+		<link  rel="stylesheet" type="text/css" href="content/css/overrides.css" />
+		<script type="text/javascript" src="content/bootstrap/js/bootstrap.min.js"></script>
 	</head>
 	<body>
+		<?php include 'header.html';?>
+		<div>
+		</div>
 	</body>
 </html>
