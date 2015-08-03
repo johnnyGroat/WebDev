f7872920fae2f810ef84a00ed120d7a8dafff441
Johnny Groat <johnathan.groat@gmail.com>
2015-08-02 23:20:40 -0400

    Post resume to index page

--- index.php
+++ index.php
@@ -1,67 +1,290 @@
 <html>
 	<head>
 		<?php include 'header.html';?>
-		<script type="text/javascript" src="content/slides/jquery.slides.min.js"></script>
-		
-  <script>
-    $(function(){
-      $("#_about-slider").slidesjs({
-        width: 800,
-        height: 600,
-        navigation: {
-        	active: false,
-        	effect: "fade"
-        },
-        pagination: {
-        	active: false
-        },
-        play: {
-          active: false
-
-        }
-      });
-    });
-  </script>
-		<style type="text/css">
-			#_about-slider {
-				display:none;
-			}
-		</style>
 	</head>
 	<body>
 		<div class="body-content">
 			<div class="container-fluid">
 				<div class="panel panel-default">
 					<div class="panel-heading background-gradient">
-						Welcome!
+						Work Experience
 					</div>
 					<div class="panel-body">
-						<div>
-							This is my work-in-progress development site where I plan on posting interesting finds within the web development world along with any cool and interesting projects that I have worked on or ideas that I have.
+						<div class="row employment-row">
+							<div class="col-xs-3 org-img" style="min-width: 36px;"><img src="https://www.relsci.com/Content/External/Images/logo-red.png" style="height:36px" />
+							</div>
+							<div class="col-xs-5 work-title">
+								Software/Database Engineer at <a href="https://www.relsci.com" target="_blank">RelSci</a>
+							</div>
+							<div class="col-xs-3 work-date">June 2013 - Present</div>
+						</div>
+						<ul class="list-group">
+							<li class="list-group-item">
+								Built and maintained client facing functionality using C# .NET MVC RAZOR framework along with SQL and jQuery components
+							</li>
+							<li class="list-group-item">
+								Built and maintained SalesForce managed package for web and mobile utilizing Force.com APEX code integrated with RelSci REST API methods
+							</li>
+							<li class="list-group-item">
+								Integrated 3rd party wiki software to effectively store evolving data governance 
+							</li>
+							<li class="list-group-item">
+								Analyzed and modified automated data entity standardization procedures using SQL CLR to ensure proper business logic execution
+							</li>
+							<li class="list-group-item">
+								Helped train existing employees regarding best SQL practices by creating presentations and demo code
+							</li>
+						</ul>
+					</div>
+					<div class="panel-body">
+						<div class="row employment-row">
+							<div class="col-xs-3 org-img" style="min-width: 140px;">
+								<div class="ciq-background" style="min-width: 140px;">
+									<img style="background-color:#3A3A3A;" src="https://www.capitaliq.com/images/logo-CIQ.png" style="height:36px" />
+								</div>
+							</div>
+							<div class="col-xs-5 work-title">
+								Database Engineer III at
+								<a href="https://www.capitaliq.com" target="_blank">S&P Capital IQ</a>
+							</div>
+							<div class="col-xs-3 work-date">Jan 2011 - June 2013</div>
+						</div>
+						<ul class="list-group">
+							<li class="list-group-item">
+								Analyzed complex financial data systems stored in Microsoft SQL Server to ensure optimal SQL procedure and job performance, data integrity, and data accuracy
+							</li>
+							<li class="list-group-item">
+								Estimated key deliverable dates for our team as well as provide other teams with potential technical, system, or personal road-blocks that prevent project completion.
+							</li>
+							<li class="list-group-item">
+								Part of 24/7 production support team that maintained system up-time as well as overall database environment health
+							</li>
+							<li class="list-group-item">
+								Designed and developed new data functionality that integrates large amounts of data into complex database schemas 
+							</li>
+							<li class="list-group-item">
+								Developed and enhanced database deployment system for moving data and database code into development, testing, and production environments
+							</li>
+							<li class="list-group-item">
+								Helped design new training program to accelerate new hire learning curve teaching basic and advanced SQL skills, organizational coding practices, design processes, and provided mentorship to college hires
+							</li>
+						</ul>
+					</div>
+					<div class="panel-body">
+						<div class="row employment-row">
+							<div class="col-xs-3 org-img" style="min-width: 140px;">
+								<div class="ciq-background" style="min-width: 140px;">
+									<img style="background-color:#3A3A3A;" src="https://www.capitaliq.com/images/logo-CIQ.png" style="height:36px" />
+								</div>
+							</div>
+							<div class="col-xs-5 work-title">
+								Software Engineer Intern at
+								<a href="https://www.capitaliq.com" target="_blank">S&P Capital IQ</a>
+							</div>
+							<div class="col-xs-3 work-date">May 2010 - Aug 2010</div>
+						</div>
+						<ul class="list-group">
+							<li class="list-group-item">
+								Designed and Developed front-end and back-end applications used for quality assurance and data management
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading background-gradient">
+						Competencies 
+					</div>
+					<div class="panel-body">
+						<ul class="list-group"> 
+							<li class="list-group-item">
+								<div class="row">
+									<div class="col-xs-3">T-SQL
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+									</div>
+									<div class="col-xs-3">PL-SQL
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+								</div>
+							</li>
+							<li class="list-group-item">
+								<div class="row">
+									<div class="col-xs-3">SSIS
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+									<div class="col-xs-3">Windows PowerShell
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+								</div>
+							</li>
+							<li class="list-group-item">
+								<div class="row">
+									<div class="col-xs-3">Java
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+									<div class="col-xs-3">C/C++
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+								</div>
+							</li>
+							<li class="list-group-item">
+								<div class="row">
+									<div class="col-xs-3">C#
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+									<div class="col-xs-3">HTML
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+								</div>
+							</li>
+							<li class="list-group-item">
+								<div class="row">
+									<div class="col-xs-3">CSS
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+									<div class="col-xs-3">JavaScript
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+								</div>
+							</li>
+							<li class="list-group-item">
+								<div class="row">
+									<div class="col-xs-3">JQuery
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+									<div class="col-xs-3">Salesforce APEX
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+										<span class="glyphicon glyphicon-star-empty"></span>
+									</div>
+								</div>
+							</li>
+							<li class="list-group-item">
+								<div class="row">
+									<div class="col-xs-3">Git
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-ok"></span>
+									</div>
+									<div class="col-xs-3">Tortoise SVN
+									</div>
+									<div class="col-xs-3">
+										<span class="glyphicon glyphicon-ok"></span>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading background-gradient">
+						Education
+					</div>
+					<div class="panel-body">
+						<div class="row employment-row">
+							<div class="col-xs-3 org-img" style="min-width: 36px;"><img src="http://www.setonhill.edu/media/images/test_footer.png" style="height:36px" />
+							</div>
+							<div class="col-xs-5 work-title">
+								Bachelors of Science degree in Computer Science at <a href="https://www.setonhill.edu" target="_blank">Seton Hill University</a>
+							</div>
+							<div class="col-xs-3 work-date">Aug 2006 - Dec 2010</div>
 						</div>
 					</div>
 				</div>
 				<div class="panel panel-default">
 					<div class="panel-heading background-gradient">
-						About
+						Contact
 					</div>
 					<div class="panel-body">
-						I'm a Michigan native and have started my professional career in New York City and currently live in Brooklyn. I like to fill my personal life with all sorts of activities which include travel, running, playing and watching sports (go Red Wings/Tigers/Lions/Pistons!).
-					</div>
-					<div class="panel-body">
-						<div id="_about-slider">
-							<img src="content/images/about/brothersHockey.jpg">
-							<img src="content/images/about/mtWashingtonSmall.png">
-							<img src="content/images/about/trio.jpg">
-							<img src="content/images/about/collegeLacrosse.jpg">
-							<img src="content/images/about/romesmall.jpg">
-							<img src="content/images/about/rangersRedwings.jpg">
-							<img src="content/images/about/belmont.jpg">
-							<img src="content/images/about/baldmtnsmall.png">
-							<span class="slidesjs-previous slidesjs-navigation glyphicon glyphicon-chevron-left" style="padding-top:10px;" aria-hidden="true">
-							</span>
-							<span class="slidesjs-next slidesjs-navigation glyphicon glyphicon-chevron-right" style="padding-top:10px;" aria-hidden="true">
-							</span>
+						<div class="row  work-title">
+							<div style="text-align: center;">
+								Johnathan J. Groat
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-6">
+								johnathan.groat@gmail.com
+							</div>
+							<div class="col-xs-6 address-align">
+								155 7th Ave. #1
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-6">
+								(734) 612-0189
+							</div>
+							<div class="col-xs-6 address-align">
+								Brooklyn, NY 11215
+							</div>
 						</div>
 					</div>
 				</div>
